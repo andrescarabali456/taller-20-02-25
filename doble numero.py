7e79ab44dde3fc74d070f2cb0f45a6b0b7d0411c
andrescarabali456 <andres.carabali01@usa.edu.co>
2025-02-20 16:31:53 -0500

    Add files via upload

---
+++ doble numero.py
@@ -0,0 +1,15 @@
+
+numero = float(input())
+
+
+doble = numero * 2
+
+
+print(doble)
+
+print(""" para obtener el doble de un  numero
+      primero sebo darle un proposito a la variable numero
+      a esta, dentro  de la variable doble , se le multiplica por dos
+      se pide que se muestre la variable doble en pantalla 
+      acto seguido nos pide el numero del cual queremos el doble
+      se obtiene el numero""")
