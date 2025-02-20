1cf9904c39b24a8811269918916afe0773593a23
andrescarabali456 <andres.carabali01@usa.edu.co>
2025-02-20 16:56:05 -0500

    Add files via upload

---
+++ ejemplos.py
@@ -0,0 +1,38 @@
+numero = float(input())
+#aqui es la entradra ya que es donde se lee el numero ingresado
+
+doble = numero * 2
+#este es el proceso ya que es donde se multiplica el numero por 2
+
+print(doble)
+#esta es la salida ya que es donde se muestra el resultado
+
+#ejemplo 1
+
+x = float(input())
+triple = x * 3
+print(triple)
+
+#ejemplo 2
+
+y = float(input())
+mitad = y / 2
+print(mitad)
+
+#ejemplo 3
+
+z = float(input())
+potencia = z ** 2
+print(potencia)
+
+#ejemplo 4 
+
+a = float(input())
+residuo = a % 4
+print(residuo)
+
+#ejemplo 5
+
+b = float(input())
+aumentado = b + 10 
+print(aumentado)
